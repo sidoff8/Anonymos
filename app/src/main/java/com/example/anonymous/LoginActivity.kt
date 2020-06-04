0c02313ced878f4dfb71abe4599559f163f3e271
Sidharth Singh <sidharthsingh1806@gmail.com>
2020-06-04 07:01:16 +0530

    added reg log and rounded buttons

--- app/src/main/java/com/example/anonymous/LoginActivity.kt
+++ app/src/main/java/com/example/anonymous/LoginActivity.kt
@@ -13,17 +13,35 @@ class LoginActivity: AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val email= email_editText_login.text.toString()
-        val password= password_editText_login.text.toString()
-
         //firebase login
-
+        login_button.setOnClickListener {
+            performLogin()
+        }
 
 
         dont_havr_account_textView.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+    }
+    private fun performLogin() {
+        val email = email_editText_login.text.toString()
+        val password = password_editText_login.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill out email/pw.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                Log.d("Login", "Successfully logged in: ${it.result?.user?.uid}")
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Failed to log in: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 
 }
